aa5146fed71de7ed2d63153a127c18f79c479fb8
Kim Kyeongho <sgn07124@gmail.com>
2024-08-24 02:38:23 +0900

    initial project setting

---
+++ src/test/java/study/data_jpa/controller/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package study.data_jpa.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloControllerTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
