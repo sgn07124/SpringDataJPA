09fc563b8be3d6968147e95b144adc4fb7166a4a
Kim Kyeongho <sgn07124@gmail.com>
2024-08-30 00:41:50 +0900

    5.3 Query, 리포지토리 메소드에 쿼리 정의

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -13,4 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     //@Query(name = "Member.findByUsername")  // 이게 없어도 "메소드 이름으로 쿼리 생성"을 우선적으로 처리하기 때문에 생략이 가능하다
     List<Member> findByUsername(@Param("username") String username);  // m.username = :username 처럼 명확하게 JPQL을 작성했을 때 @Param으로 값을 넘겨야함
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")  // 애플리케이션 로딩 시점에 파싱을 진행함 -> 오류가 있으면 실행이 멈춤
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
 }
