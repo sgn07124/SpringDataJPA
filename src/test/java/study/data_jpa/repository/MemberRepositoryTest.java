340490cc1e579133d42074e5bf46cf1a01aeba43
Kim Kyeongho <sgn07124@gmail.com>
2024-08-30 02:18:28 +0900

    5.6 반환 타입

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.data_jpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -140,5 +141,24 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //List<Member> aaa = memberRepository.findListByUsername("AAA");
+
+        //Member findMember = memberRepository.findMemberByUsername("BBB");
+        //System.out.println("findMember = " + findMember);
+
+        //Optional<Member> findOpMember = memberRepository.findOptionalByUsername("AAA");
+        //System.out.println("findOpMember = " + findOpMember);
+
+        Optional<Member> findMember = memberRepository.findOptionalByUsername("adfasdf");  // 데이터가 있을 수도 있고 없을 수도 있다면 Optional<>을 쓸 것!!
+        System.out.println("findMember = " + findMember);  // findMember = Optional.empty
     }
 }
