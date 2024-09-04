87ab23710c8d471ea939203dd4ecc4c71a7fdd87
Kim Kyeongho <sgn07124@gmail.com>
2024-09-04 23:53:21 +0900

    6.2 Auditing

---
+++ src/main/java/study/data_jpa/entity/JpaBaseEntity.java
@@ -0,0 +1,30 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class JpaBaseEntity {
+
+    @Column(updatable = false)  // 변경되면 안됨
+    private LocalDateTime createDate;  // 생성일
+    private LocalDateTime updatedDate;  // 수정일
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
+}
